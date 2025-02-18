dae94af3f5983cab43bc525d0187ec32bd78799c
Marcus Koehler <khler.marcus@gmail.com>
2025-02-18 16:03:52 +0100

    Add java_class mod

--- src/parsing/java_class/src/lib.rs
+++ src/parsing/java_class/src/lib.rs
@@ -1,130 +1,132 @@
-use binrw::prelude::*;
+pub mod java_class {
+    use binrw::prelude::*;
 
-#[binread]
-#[br(magic = 0xCAFEBABEu32)]
-pub struct Class {
-    min_ver: u16,
-    maj_ver: u16,
-    #[br(temp)]
-    const_pool_count: u16,
-    #[br(count = const_pool_count)]
-    const_pool: Vec<ConstPoolEntry>,
-    access_modifiers: u16, //bitfield
-    this_class_idx: u16,
-    super_class_idx: u16,
-    #[br(temp)]
-    iface_count: u16,
-    #[br(count = iface_count)]
-    iface_indexes: Vec<u16>,
-    #[br(temp)]
-    fields_count: u16,
-    #[br(count = fields_count)]
-    fields: Vec<FieldInfo>,
-    #[br(temp)]
-    methods_count: u16,
-    #[br(count = methods_count)]
-    methods: Vec<MethodInfo>,
-    #[br(temp)]
-    attr_count: u16,
-    #[br(count = attr_count)]
-    attributes: Vec<AttributeInfo>,
-}
-
-fn read_utf8_lossy(data: Vec<u8>) -> String {
-    match String::from_utf8(data) {
-        Ok(s) => s,
-        Err(_) => "N/A".to_owned(),
+    #[binread]
+    #[br(magic = 0xCAFEBABEu32)]
+    pub struct Class {
+        min_ver: u16,
+        maj_ver: u16,
+        #[br(temp)]
+        const_pool_count: u16,
+        #[br(count = const_pool_count)]
+        const_pool: Vec<ConstPoolEntry>,
+        access_modifiers: u16, //bitfield
+        this_class_idx: u16,
+        super_class_idx: u16,
+        #[br(temp)]
+        iface_count: u16,
+        #[br(count = iface_count)]
+        iface_indexes: Vec<u16>,
+        #[br(temp)]
+        fields_count: u16,
+        #[br(count = fields_count)]
+        fields: Vec<FieldInfo>,
+        #[br(temp)]
+        methods_count: u16,
+        #[br(count = methods_count)]
+        methods: Vec<MethodInfo>,
+        #[br(temp)]
+        attr_count: u16,
+        #[br(count = attr_count)]
+        attributes: Vec<AttributeInfo>,
     }
-}
 
-#[binread]
-pub enum ConstPoolEntry {
-    #[br(magic = 0x07u8)]
-    Class { name_index: u16 },
-    #[br(magic = 0x09u8)]
-    FieldRef {
-        class_index: u16,
-        name_type_index: u16,
-    },
-    #[br(magic = 0x0Au8)]
-    MethodRef {
-        class_index: u16,
-        name_type_index: u16,
-    },
-    #[br(magic = 0x0Bu8)]
-    IfaceMethodRef {
-        class_index: u16,
-        name_type_index: u16,
-    },
-    #[br(magic = 0x08u8)]
-    String { index: u16 },
-    #[br(magic = 0x03u8)]
-    Int { value: i32 },
-    #[br(magic = 0x04u8)]
-    Float { value: f32 },
-    #[br(magic = 0x05u8)]
-    Long { value: i64 },
-    #[br(magic = 0x06u8)]
-    Double { value: f64 },
-    #[br(magic = 0x0Cu8)]
-    NameAndType {
+    fn read_utf8_lossy(data: Vec<u8>) -> String {
+        match String::from_utf8(data) {
+            Ok(s) => s,
+            Err(_) => "N/A".to_owned(),
+        }
+    }
+
+    #[binread]
+    pub enum ConstPoolEntry {
+        #[br(magic = 0x07u8)]
+        Class { name_index: u16 },
+        #[br(magic = 0x09u8)]
+        FieldRef {
+            class_index: u16,
+            name_type_index: u16,
+        },
+        #[br(magic = 0x0Au8)]
+        MethodRef {
+            class_index: u16,
+            name_type_index: u16,
+        },
+        #[br(magic = 0x0Bu8)]
+        IfaceMethodRef {
+            class_index: u16,
+            name_type_index: u16,
+        },
+        #[br(magic = 0x08u8)]
+        String { index: u16 },
+        #[br(magic = 0x03u8)]
+        Int { value: i32 },
+        #[br(magic = 0x04u8)]
+        Float { value: f32 },
+        #[br(magic = 0x05u8)]
+        Long { value: i64 },
+        #[br(magic = 0x06u8)]
+        Double { value: f64 },
+        #[br(magic = 0x0Cu8)]
+        NameAndType {
+            name_index: u16,
+            descriptor_index: u16,
+        },
+        #[br(magic = 0x01u8)]
+        Utf8 {
+            #[br(temp)]
+            length: u16,
+            #[br(count = length, map = |s: Vec<u8>| read_utf8_lossy(s))]
+            value: String,
+        },
+        #[br(magic = 0x0Eu8)]
+        MethodHandle { ref_kind: u8, ref_index: u16 },
+        #[br(magic = 0x0Fu8)]
+        MethodType { descriptor_index: u16 },
+        #[br(magic = 0x12u8)]
+        InvokeDynamic {
+            #[br(ignore)]
+            bootstrap_index: u16,
+            name_type_index: u16,
+        },
+    }
+
+    #[binread]
+    pub struct FieldInfo {
+        flags: u16, //bitfield
         name_index: u16,
         descriptor_index: u16,
-    },
-    #[br(magic = 0x01u8)]
-    Utf8 {
         #[br(temp)]
-        length: u16,
-        #[br(count = length, map = |s: Vec<u8>| read_utf8_lossy(s))]
-        value: String,
-    },
-    #[br(magic = 0x0Eu8)]
-    MethodHandle { ref_kind: u8, ref_index: u16 },
-    #[br(magic = 0x0Fu8)]
-    MethodType { descriptor_index: u16 },
-    #[br(magic = 0x12u8)]
-    InvokeDynamic {
-        #[br(ignore)]
-        bootstrap_index: u16,
-        name_type_index: u16,
-    },
-}
+        attributes_count: u16,
+        #[br(count = attributes_count)]
+        attributes: Vec<AttributeInfo>,
+    }
 
-#[binread]
-pub struct FieldInfo {
-    flags: u16, //bitfield
-    name_index: u16,
-    descriptor_index: u16,
-    #[br(temp)]
-    attributes_count: u16,
-    #[br(count = attributes_count)]
-    attributes: Vec<AttributeInfo>,
-}
+    #[binread]
+    pub struct MethodInfo {
+        flags: u16, //bitfield
+        name_index: u16,
+        descriptor_index: u16,
+        #[br(temp)]
+        attributes_count: u16,
+        #[br(count = attributes_count)]
+        attributes: Vec<AttributeInfo>,
+    }
 
-#[binread]
-pub struct MethodInfo {
-    flags: u16, //bitfield
-    name_index: u16,
-    descriptor_index: u16,
-    #[br(temp)]
-    attributes_count: u16,
-    #[br(count = attributes_count)]
-    attributes: Vec<AttributeInfo>,
-}
+    #[binread]
+    pub struct AttributeInfo {
+        name_index: u16,
+        #[br(temp)]
+        length: u32,
+        #[br(count = length)]
+        data: Vec<u8>,
+    }
 
-#[binread]
-pub struct AttributeInfo {
-    name_index: u16,
-    #[br(temp)]
-    length: u32,
-    #[br(count = length)]
-    data: Vec<u8>,
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert!(true);
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn it_works() {
+            assert!(true);
+        }
     }
 }
