b4e3c218491b7f8442e202713ff92cfdabb1d278
Marcus Koehler <khler.marcus@gmail.com>
2025-02-24 13:11:26 +0100

    Basic extraction of required and provided classes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use binrw::BinReaderExt;
+use import_checker::{Consumer, Provider};
 use std::{env, fs::File};
 
 use java_class::java_class::Class;
@@ -7,5 +8,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let mut classfile = File::open(&args[1]).expect("Failed to open file!");
     let parsed: Class = classfile.read_be().unwrap();
-    println!("{:?}", parsed)
+    let parsed_requires = parsed.get_consumed();
+    let parsed_provides = parsed.get_provided();
+    println!("Consumes: {:?}", parsed_requires);
+    println!("Provides: {:?}", parsed_provides);
 }
--- src/parsing/import_checker/src/lib.rs
+++ src/parsing/import_checker/src/lib.rs
@@ -1,14 +1,51 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use java_class::java_class::{Class, ConstPoolEntry};
+use regex::Regex;
+
+pub trait Consumer {
+    fn get_consumed(&self) -> Vec<String>;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait Provider {
+    fn get_provided(&self) -> Vec<String>;
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn get_references(candidate: &str) -> Vec<String> {
+    let re = Regex::new(r"^((?:[[:alnum:]]+/)+[[:alnum:]]+)").expect("Invalid Regex!");
+    let mut result = vec![];
+    if let Some(caps) = re.captures(candidate) {
+        result.push(caps[0].to_owned());
+    }
+    result
+}
+
+fn get_const_pool_entry(pool: &[ConstPoolEntry], idx: usize) -> &ConstPoolEntry {
+    &pool[idx - 1]
+}
+
+impl Consumer for Class {
+    fn get_consumed(&self) -> Vec<String> {
+        let mut imports: Vec<String> = vec![];
+        for cp_info in &self.const_pool {
+            if let ConstPoolEntry::Utf8 { value } = cp_info {
+                imports.append(&mut get_references(value));
+            }
+        }
+        imports
+    }
+}
+
+impl Provider for Class {
+    fn get_provided(&self) -> Vec<String> {
+        let mut result = vec![];
+        if let &ConstPoolEntry::Class { name_index } =
+            get_const_pool_entry(&self.const_pool, self.this_class_idx as usize)
+        {
+            if let ConstPoolEntry::Utf8 { value } =
+                get_const_pool_entry(&self.const_pool, name_index as usize)
+            {
+                result.push(value.to_owned());
+            }
+        }
+        result
     }
 }
