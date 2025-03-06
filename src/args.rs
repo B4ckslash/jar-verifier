a3ad8cfbb9993dff09ce29f14caad641c9c34022
Marcus Koehler <khler.marcus@gmail.com>
2025-03-06 14:30:27 +0100

    Add class info for Java 17; add positional argument for JDK classinfo

--- src/args.rs
+++ src/args.rs
@@ -5,6 +5,8 @@ use clap::Parser;
 pub struct Args {
     ///Classpath of JARs to be checked
     pub classpath: String,
+    ///A file listing the available classes and methods of the relevant JDK
+    pub jdk_classinfo: String,
     ///Whether the program runs in parallel
     #[arg(short, long, default_value_t = false)]
     pub parallel: bool,
