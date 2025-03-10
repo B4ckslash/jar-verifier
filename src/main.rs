e42777715e6c9dd2e2384f8c654c95cae459305e
Marcus Koehler <khler.marcus@gmail.com>
2025-03-10 21:52:36 +0100

    Check result of java class info read earlier

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,10 @@ use reference_checker::check_classes;
 fn main() -> Result<(), error::Error> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let args = Args::parse();
-    let java_classes = read_classinfo(&args.jdk_classinfo);
+    let java_classes = read_classinfo(&args.jdk_classinfo)?;
     let classes = parse_classpath(&args.classpath, args.parallel)?;
     let consumed =
-        check_classes(&classes, args.parallel, &java_classes?).expect("Failed to get result");
+        check_classes(&classes, args.parallel, &java_classes).expect("Failed to get result");
     println!(
         "Classpath: {} \n Class count {} \n Consume count: {:?}",
         &args.classpath,
